2115d0ce25c52f7e87fe389efa901ccf93a81df9
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-03 18:00:56 -0500

    Se configuraron las DevTools del navegado

--- src/store/store.js
+++ src/store/store.js
@@ -5,4 +5,7 @@ const reducers = combineReducers({
   auth: authReducer,
 });
 
-export const store = createStore(reducers);
+export const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
