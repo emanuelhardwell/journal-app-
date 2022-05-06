d5708f6f1d76e8a28c4babd2b63d7df56994f1f2
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-06 11:10:38 -0500

    [note] Se creo funcionalidad para activar la nota para su edicion

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,9 +1,17 @@
 import dayjs from "dayjs";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { activeNote } from "../../actions/notes";
 
 export const JournalEntry = ({ id, title, body, date, url }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(activeNote(id, { title, body, date, url }));
+  };
+
   return (
-    <div className="journal__entry pointer">
+    <div className="journal__entry pointer" onClick={handleClick}>
       {url && (
         <div
           className="journal__entry-picture"
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar";
 
 export const NoteScreen = () => {
+  const note = useSelector((state) => state.notes.active);
+
+  const [formValues, handleInputChange] = useForm(note);
+  const { title, body } = formValues;
+
   return (
     <div className="notes__main-content">
       <NotesAppBar />
@@ -12,19 +19,25 @@ export const NoteScreen = () => {
           placeholder="Some awesome title"
           className="notes__title-input"
           autoComplete="off"
+          value={title}
+          onChange={handleInputChange}
         />
 
         <textarea
           placeholder="What happened today"
           className="notes__textarea"
+          value={body}
+          onChange={handleInputChange}
         ></textarea>
 
-        <div className="notes__image">
-          <img
-            src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-            alt="imagen"
-          />
-        </div>
+        {formValues.url && (
+          <div className="notes__image">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+              alt="imagen"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
