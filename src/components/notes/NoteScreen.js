39f7130ef0ca01d524b27b23a6208ed313704568
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-06 11:34:55 -0500

    [note] se modifico reset() del hook useForm y se uso el hook useRef para tener la referencia al ID de la nota activa y mostrarla

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar";
@@ -6,8 +6,17 @@ import { NotesAppBar } from "./NotesAppBar";
 export const NoteScreen = () => {
   const note = useSelector((state) => state.notes.active);
 
-  const [formValues, handleInputChange] = useForm(note);
+  const [formValues, handleInputChange, reset] = useForm(note);
   const { title, body } = formValues;
+
+  const activeIdRef = useRef(note.id);
+
+  useEffect(() => {
+    if (note.id !== activeIdRef.current) {
+      reset(note);
+      activeIdRef.current = note.id;
+    }
+  }, [note, reset]);
 
   return (
     <div className="notes__main-content">
@@ -30,7 +39,7 @@ export const NoteScreen = () => {
           onChange={handleInputChange}
         ></textarea>
 
-        {formValues.url && (
+        {note.url && (
           <div className="notes__image">
             <img
               src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 export const useForm = (initialState = {}) => {
   const [values, setValues] = useState(initialState);
 
-  const reset = () => {
-    setValues(initialState);
+  const reset = (newValues = initialState) => {
+    setValues(newValues);
   };
 
   const handleInputChange = ({ target }) => {
