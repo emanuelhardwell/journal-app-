62fa7ea795fadffa0f9509d517108523ffabdf8c
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-07 13:38:51 -0500

    [note] se creo funcionalidad para borrar una nota

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -96,3 +96,19 @@ export const startUploading = (file) => {
     Swal.close();
   };
 };
+
+export const startDeleteNote = (id) => {
+  return async (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    await db.doc(`${uid}/journal/notes/${id}`).delete();
+
+    dispatch(deleteNote(id));
+  };
+};
+
+export const deleteNote = (id) => {
+  return {
+    type: types.notesDelete,
+    payload: id,
+  };
+};
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { activeNote } from "../../actions/notes";
+import { activeNote, startDeleteNote } from "../../actions/notes";
 import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar";
 
@@ -10,7 +10,7 @@ export const NoteScreen = () => {
   const note = useSelector((state) => state.notes.active);
 
   const [formValues, handleInputChange, reset] = useForm(note);
-  const { title, body } = formValues;
+  const { title, body, id } = formValues;
 
   const activeIdRef = useRef(note.id);
 
@@ -24,6 +24,10 @@ export const NoteScreen = () => {
   useEffect(() => {
     dispatch(activeNote(formValues.id, { ...formValues }));
   }, [formValues, dispatch]);
+
+  const handleDelete = () => {
+    dispatch(startDeleteNote(id));
+  };
 
   return (
     <div className="notes__main-content">
@@ -54,6 +58,10 @@ export const NoteScreen = () => {
           </div>
         )}
       </div>
+
+      <button className="btn btn-danger" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 };
